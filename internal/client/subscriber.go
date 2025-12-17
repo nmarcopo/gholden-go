b94d70789e7a08bf915c446b783e4cb0e85e0147
Nicholas Marcopoli <nick@marcopo.li>
2025-12-16 18:44:04 -1000

    Wire up a subscriber for receiving messages (#4)

---
+++ internal/client/subscriber.go
@@ -0,0 +1,50 @@
+package client
+
+import (
+	"context"
+	"gholden-go/internal/parser"
+	"log/slog"
+	"time"
+
+	"github.com/coder/websocket"
+	"github.com/pkg/errors"
+)
+
+type subscriber struct {
+	queue   chan<- *parser.Showdown
+	logger  *slog.Logger
+	timeout time.Duration
+}
+
+func newSubscriber(queue chan<- *parser.Showdown, logger *slog.Logger, timeout time.Duration) *subscriber {
+	return &subscriber{
+		queue:   queue,
+		logger:  logger,
+		timeout: timeout,
+	}
+}
+
+func (p *subscriber) run(ctx context.Context, conn *websocket.Conn) error {
+	for {
+		msgType, msg, err := conn.Read(ctx)
+		if err != nil {
+			p.logger.WarnContext(ctx, "websocket read error", "error", errors.WithStack(err))
+			continue
+		}
+		if msgType != websocket.MessageText {
+			p.logger.WarnContext(ctx, "websocket message type not supported", "message type", msgType)
+			continue
+		}
+
+		parsed, err := parser.ShowdownParser.Parse(msg)
+		if err != nil {
+			p.logger.WarnContext(ctx, "message parse error", "error", err)
+			continue
+		}
+		select {
+		case p.queue <- parsed:
+		case <-ctx.Done():
+			return errors.WithStack(ctx.Err())
+		}
+	}
+}
